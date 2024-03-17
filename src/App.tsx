8cc71a759d61009331b27836da88e261663ec3fc
imalanvalencia <contact@alanvalencia.me>
2024-03-17 18:28:16 +0100

    Setup project
    Setup project with shadcn for ui and vite with React TS

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import CurriculumForm from './components/curriculum-form'
+
+function App() {
+
+  return (
+    <>
+      <CurriculumForm />
+        </>
+  )
+}
+
+export default App
---
+++ src/components/curriculum-form.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CurriculumForm() {
+  return (
+    <div>
+      <h1>CurriculumForm</h1>
+      
+    </div>
+  )
+}
